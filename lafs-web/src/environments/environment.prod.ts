432f9dde70b069788f6484ebc5b7c9bfa9dc038e
xxxevilninjaxx <techgeek1998@gmail.com>
2023-04-07 14:14:03 -0400

    fixed url

--- lafs-web/src/environments/environment.prod.ts
+++ lafs-web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://s87rqwta95.execute-api.us-east-1.amazonaws.com/api'
+  api_url: 'https://s87rqwta95.execute-api.us-east-1.amazonaws.com'
 };
